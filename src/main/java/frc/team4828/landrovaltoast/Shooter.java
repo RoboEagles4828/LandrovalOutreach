1e15450b4a4768a417ba2d3599e88821f5e94b4e
Joseph Ryan <Josephryan3.14@gmail.com>
2016-09-29 11:46:17 -0400

    PID updates
    I read the entire software manual for CANTalons, they are pretty amazing
    actually would recommend 8/8

--- src/main/java/frc/team4828/landrovaltoast/Shooter.java
+++ src/main/java/frc/team4828/landrovaltoast/Shooter.java
@@ -19,13 +19,31 @@ public class Shooter {
         centerHall = Registrar.digitalInput(ch);
         servo1 = new Servo(s1);
         servo2 = new Servo(s2);
+
+        //not sure if we need motor.enableControl();
+        slaveMotor.changeControlMode(CANTalon.TalonControlMode.Follower);
+        slaveMotor.set(masterMotor.getDeviceID());
+        slaveMotor.reverseOutput(true);
+        verticalMotor.changeControlMode(CANTalon.TalonControlMode.Position);
+        verticalMotor.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder); // TODO: verify the type of encoder we have
+        refreshEncoder();  //Zeros the encoder position
+        // TODO: add command bus option to re-zero so we dont have to power cycle
         refreshPID();
+        /*
+        * soft limits can be easily set with:
+        * enableForwardSoftLimit(true), enableReverseSoftLimit(true)
+        * setForwardSoftLimit(int pos), setReverseSoftLimit(int pos)
+        * */
     }
 
     public void refreshPID() {
         verticalMotor.setP(RobotModule.config.getDouble("constants.shooter.P", 0.0));
         verticalMotor.setI(RobotModule.config.getDouble("constants.shooter.I", 0.0));
         verticalMotor.setD(RobotModule.config.getDouble("constants.shooter.D", 0.0));
+    }
+
+    public void refreshEncoder(){
+        verticalMotor.setPosition(0);
     }
 
 }
