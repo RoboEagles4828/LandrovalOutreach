74e8a6937f7ac0018af13f1efdcd4c816c8d944b
Jackie Chen <cheezballz3.14@gmail.com>
2016-10-06 20:05:12 -0400

    JUnit Implementation
    Implementing JUnit
    
    JUnit Test 1
    
    Added a tester and class to test with
    
    JUnit Test 2
    
    Changed something on a whim
    
    JUnit Test 3
    
    Fixed the error
    
    JUnit Test 4
    
    Got testing to happen, test does not work yet
    
    JUnit Test 5
    
    Not sure what is going wrong, tests fail in console but work in Travis. Recommitting to recheck
    
    JUnit Test 6
    
    Changed build.gradle a bit
    
    Fixed a Typo
    
    JUnit Test 7
    
    Added junit to .travis file so that it installs junit
    
    JUnit Test 8
    
    Moved test before verify cuz verify takes too long
    
    JUnit Test 8
    
    Added RunWith in Tester
    
    JUnit Test 9
    
    Will it work? Only one way to find out.

---
+++ src/test/java/Tester.java
@@ -0,0 +1,17 @@
+import frc.team4828.landrovaltoast.TestClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+@RunWith(JUnit4.class)
+public class Tester {
+    @Test
+    public void test() {
+        System.out.println("You're currently testing Tester!");
+        TestClass test = new TestClass();
+        int returned = test.testTester(3);
+        System.out.println("Number: " + returned);
+        assertEquals(6, returned);
+    }
+}
