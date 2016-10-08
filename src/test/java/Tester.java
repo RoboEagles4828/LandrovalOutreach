6a1fc746098b3eb53a6256d97c0b2665f2095af4
Jackie Chen <cheezballz3.14@gmail.com>
2016-10-08 11:52:40 -0400

    Logger Test
    Testing Java's logger

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -2,16 +2,23 @@ import frc.team4828.landrovaltoast.TestClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
+
+/**
+ * This class is used for testing things on the robot.
+ */
+
 @RunWith(JUnit4.class)
 public class Tester {
     @Test
-    public void test() {
-        System.out.println("You're currently testing Tester!");
+    public void sampleTest() {
+        Logger.getLogger("InfoLog").log(Level.INFO, "You're currently testing Tester!");
         TestClass test = new TestClass();
         int returned = test.testTester(3);
-        System.out.println("Number: " + returned);
+        Logger.getLogger("InfoLog").log(Level.INFO, "Number: " + returned);
         assertEquals(6, returned);
     }
 }
