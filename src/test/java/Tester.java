7bc8ce58b335ac7c9ccdab8217e781307cb50ffa
Jackie Chen <cheezballz3.14@gmail.com>
2016-10-10 12:36:58 -0400

    Remove Sample Tester

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,12 +1,9 @@
 import frc.team4828.landrovaltoast.Drive;
-import frc.team4828.landrovaltoast.TestClass;
-import jaci.openrio.toast.lib.registry.Registrar;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * This class is used for testing things on the robot.
@@ -14,14 +11,6 @@ import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class Tester {
-    @Test
-    public void sampleTest() {
-        Logger.getLogger("InfoLog").log(Level.INFO, "You're currently testing Tester!");
-        TestClass test = new TestClass();
-        int returned = test.testTester(3);
-        Logger.getLogger("InfoLog").log(Level.INFO, "Number: " + returned);
-        assertEquals(6, returned);
-    }
     @Test
     public void testDrive() {
         int[] expected = {1,2,3,4};
